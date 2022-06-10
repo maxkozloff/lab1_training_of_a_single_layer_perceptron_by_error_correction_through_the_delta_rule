501c83b1ae04eae59061f4fc1e31cff326391f0b
maxkozloff <62160846+maxkozloff@users.noreply.github.com>
2022-06-10 19:32:38 +0300

    Update neuron.py

--- pure_python/neuron.py
+++ pure_python/neuron.py
@@ -1,6 +1,7 @@
 from random import random
 from math import exp
 
+
 class Neuron:
     def __init__(self, weights_count):
         self.__weights = [0] * (weights_count + 1)
@@ -8,31 +9,41 @@ class Neuron:
         self.__net = 0.0
         self.__rangeMin = - 0.0003
         self.__rangeMax = 0.0003
-        self.randomize_weights()
+        # self.randomize_weights()
+
+    def set_weights(self, weights):
+        self.__weights = weights
 
     def randomize_weights(self):
         for i in range(len(self.__weights)):
             self.__weights[i] = self.__rangeMin + (self.__rangeMax - self.__rangeMin) * random()
 
-
     def activation_func(self):
-        if self.__net > 0:
-            return 1.0
-        else:
-            return 0.0
+        return 1.0 / (1.0 + exp(-self.__net))
+
+    def delta_rule_activation_func(self, value):
+        return 1.0 if value >= 0 else 0.0
+
+    def delta_calc_y(self, x):
+        sum = 0
+        for i, signal in enumerate(x):
+            sum += signal * self.__weights[i]
+        self.__y = self.delta_rule_activation_func(sum)
 
     def derivative(self):
         return self.activation_func() * (1.0 - self.activation_func())
 
     def calc_y(self, x):
-        sum = 0;
+        self.__net = self.__weights[0]
         for i in range(len(x)):
             self.__net += x[i] * self.__weights[i + 1]
-            sum += x[i]*self.__weight[i];
-        self.__y = self.activation_func(sum)
+        self.__y = self.activation_func()
 
     def get_y(self):
         return self.__y
+
+    def get_net(self):
+        return self.__net
 
     def get_weights(self):
         return self.__weights[1:]
@@ -43,17 +54,3 @@ class Neuron:
     def correct_weights(self, weights_deltas):
         for i in range(len(self.__weights)):
             self.__weights[i] += weights_deltas[i]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
